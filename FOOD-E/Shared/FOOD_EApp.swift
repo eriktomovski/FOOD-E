193648e0a69f3a2105892c2e67b01ab4057d1399
eriktomovski <97212301+eriktomovski@users.noreply.github.com>
2022-01-09 17:41:03 -0500

    Created File

---
+++ FOOD-E/Shared/FOOD_EApp.swift
@@ -0,0 +1,17 @@
+//
+//  FOOD_EApp.swift
+//  Shared
+//
+//  Created by Erik Tomovski on 2022-01-09.
+//
+
+import SwiftUI
+
+@main
+struct FOOD_EApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
